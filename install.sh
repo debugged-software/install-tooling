a837e4400d14767647a1ce924ab42e60f1c1bfef
Chiem Janssen <chiemjanssen@Chiems-MacBook-Pro.local>
2021-11-17 16:52:29 +0100

    Updated and added back files

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ else
     brew update
 fi
 
-brew install --cask $(curl https://gist.githubusercontent.com/Choem/2da2579bdce2dff9ecb2160b2064c440/raw/casks.txt)
-brew install $(curl https://gist.githubusercontent.com/Choem/2da2579bdce2dff9ecb2160b2064c440/raw/formulae.txt)
+brew install --cask $(curl https://github.com/debugged-software/install-tooling/blob/main/casks.txt)
+brew install $(curl https://github.com/debugged-software/install-tooling/blob/main/formulae.txt)
