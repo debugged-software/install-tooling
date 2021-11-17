c92aedae872df206131b97f06690960c92a6b851
Chiem Janssen <chiemjanssen@Chiems-MacBook-Pro.local>
2021-11-17 16:32:28 +0100

    Removed txt files and updated script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which -s brew
 
-brew install $(cat formulae.txt)
-brew install --cask $(cat casks.txt)
+if [[ $? != 0 ]] ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update
+fi
+
+brew install --cask $(curl https://gist.githubusercontent.com/Choem/2da2579bdce2dff9ecb2160b2064c440/raw/casks.txt)
+brew install $(curl https://gist.githubusercontent.com/Choem/2da2579bdce2dff9ecb2160b2064c440/raw/formulae.txt)
